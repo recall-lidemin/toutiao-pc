5b08461d7f6618859a5d016d085b7237e53fe27c
recall-lidemin <1425889927@qq.com>
2020-03-04 19:28:35 +0800

    完成评论列表分页

--- src/views/contentmanage/Comment.vue
+++ src/views/contentmanage/Comment.vue
@@ -9,7 +9,7 @@
 
     <!-- 卡片视图区域 -->
     <el-card>
-      <!-- 使用封装的面包屑导航 -->
+      <!-- 使用自己封装的面包屑导航 -->
       <!-- 大量使用具名插槽，传入标签 -->
       <bread-crumb slot="header">
         <template slot="title">
@@ -20,13 +20,23 @@
         </template>
       </bread-crumb>
 
+      <!-- 搜索区域 -->
+      <el-row>
+        <el-col :span="5">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" class="input-with-select" clearable>
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+
       <!-- 表格区域 -->
       <el-table :data="commentLit" border stripe>
         <el-table-column type="index"></el-table-column>
         <el-table-column label="标题" prop="title"></el-table-column>
         <el-table-column label="评论状态" prop="comment_status" width="100">
           <template slot-scope="scope">
-            <el-switch disabled v-model="scope.row.comment_status" active-color="#13ce66" inactive-color="#ff4949">
+            <el-switch disabled v-model="scope.row.comment_status" active-color="#13ce66"
+              inactive-color="#ff4949">
             </el-switch>
           </template>
         </el-table-column>
@@ -35,10 +45,19 @@
         <el-table-column label="操作" width="300">
           <template slot-scope="scope">
             <el-button type="primary" icon="el-icon-edit" size="mini">修改</el-button>
-            <el-button type="primary" icon="el-icon-edit" size="mini" @click="commentClosed(scope.row)">{{ scope.row.comment_status ? '关闭评论' : '打开评论' }}</el-button>
+            <el-button type="primary" icon="el-icon-edit" size="mini"
+              @click="commentClosed(scope.row)">{{ scope.row.comment_status ? '关闭评论' : '打开评论' }}
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
+
+      <!-- 分页区域 -->
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+        :current-page="queryInfo.page" :page-sizes="[10, 20, 30, 40,50]"
+        :page-size="queryInfo.per_page" layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -50,10 +69,12 @@ export default {
       commentLit: [],
       // 评论列表查询对象
       queryInfo: {
+        query: '',
         page: 1,
         per_page: 10,
         response_type: 'comment'
-      }
+      },
+      total: 0
     }
   },
   created() {
@@ -63,30 +84,56 @@ export default {
     async getCommentList() {
       const res = await this.$axios.get('articles', { params: this.queryInfo })
       this.commentLit = res.data.results
+      this.total = res.data.total_count
     },
+    // 查询
+    async getArticles() {
+      const res = await this.$axios.get('search', { params: this.queryInfo })
+      this.commentLit = res.data.results
+    },
+    // 监听关闭评论事件
     async commentClosed(info) {
       const mess = info.comment_status ? '打开' : '关闭'
-      const confirmResults = await this.$confirm(`您是否${mess}该文章评论功能?`, '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).catch(err => err)
+      const confirmResults = await this.$confirm(
+        `您是否${mess}该文章评论功能?`,
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch(err => err)
       if (confirmResults !== 'confirm') {
         return this.$message.info('当前关闭操作已取消')
       }
 
       await this.$axios
-        .put(`comments/status?article_id=${info.id}`, { allow_comment: !info.comment_status })
+        .put(`comments/status?article_id=${info.id}`, {
+          allow_comment: !info.comment_status
+        })
         .catch(() => {
           this.$message.error('操作失败')
         })
 
       this.getCommentList()
       this.$message.success('更新状态成功')
+    },
+    // 监听每页数据条数
+    handleSizeChange(newPage) {
+      this.queryInfo.per_page = newPage
+      this.getCommentList()
+    },
+    // 监听当前页码
+    handleCurrentChange(num) {
+      this.queryInfo.page = num
+      this.getCommentList()
     }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.el-table{
+  margin-top: 20px;
+}
 </style>
